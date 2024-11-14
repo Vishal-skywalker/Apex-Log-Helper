c848ed0e2bf3631a480bdd9d7b1b9a869ed6d648
Vishal Das <vishal.das@mkcorp.com>
2024-11-14 11:15:52 +0530

    updated sf connection class

--- src/logList.js
+++ src/logList.js
@@ -1,19 +1,10 @@
 /* global point */
 import SFConnection from '../lib/SFConnection.js';
 
-const sfHost = getUrlSearchParam('host');
 let con = null;
 async function init() {
-    let message = await new Promise(resolve =>
-        chrome.runtime.sendMessage({ message: "getSession", sfHost }, resolve)
-    );
-    con = new SFConnection(message.hostName, message.key);
+    con = await new SFConnection().init();
     const r = await con.getUserInfo();
     point.attach('userInfo', r);
 }
 init();
-
-function getUrlSearchParam(name) {
-    const search = new URLSearchParams(window.location.search);
-    return search.get(name) ?? '';
-}
